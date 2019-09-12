d7974d3416a0996af19b2c208c2c6f51d7a4f2e7
Meestal_Richard <46305032+MeestalRichard@users.noreply.github.com>
2019-09-12 03:38:57 +0200

    Update Class1.cs
    Version 1.2
    Added the option to set a key to toggle the Radio on and off.

--- Class1.cs
+++ Class1.cs
@@ -1,25 +1,32 @@
-﻿using GTA;
+using GTA;
 using System;
 using GTA.Native;
-
+using System.Windows.Forms;
 
 namespace Disable_Radio
 {
     public class Disable_Radio : Script
     {
         private bool enabled;
+        private ScriptSettings config;
         private readonly int cheatString;
+        private Keys Toggle_Radio;
 
         public Disable_Radio()
         {
+
             enabled = Settings.GetValue("SETTINGS", "ENABLED_ON_STARTUP", true);
             cheatString = Game.GenerateHash(Settings.GetValue("SETTINGS", "CHEAT_STRING", "radio"));
+            config = ScriptSettings.Load("scripts\\Disable_Radio.ini");
+            Toggle_Radio = config.GetValue<Keys>("Options", "Toggle_Radio:", Keys.None);
 
             Interval = 0;
-            Tick += Disableradio_Tick;
+            Tick += DisableRadio_Tick;
+            KeyDown += ToggleRadio_Down;
+
         }
 
-        private void Disableradio_Tick(object sender, EventArgs e)
+        private void DisableRadio_Tick(object sender, EventArgs e)
         {
             if (Function.Call<bool>(Hash._0x557E43C447E700A8, cheatString)) //_HAS_CHEAT_STRING_JUST_BEEN_ENTERED
             {
@@ -32,7 +39,7 @@ namespace Disable_Radio
                     if (veh != null && veh.Exists() && veh.IsAlive)
                     {
                         //enable radio when disable mod
-                        Function.Call(Hash.SET_VEHICLE_RADIO_ENABLED, veh, false);
+                        Function.Call(Hash.SET_VEHICLE_RADIO_ENABLED, veh, true);
                     }
                 }
             }
@@ -43,7 +50,37 @@ namespace Disable_Radio
 
                 if (veh != null && veh.Exists() && veh.IsAlive)
                 {
-                    Function.Call(Hash.SET_VEHICLE_RADIO_ENABLED, Game.Player.Character.CurrentVehicle, true);
+                    Function.Call(Hash.SET_VEHICLE_RADIO_ENABLED, Game.Player.Character.CurrentVehicle, false);
+                }
+            }
+        }
+
+        private void ToggleRadio_Down(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Toggle_Radio)
+            {
+                enabled = !enabled;
+                UI.ShowSubtitle("~g~TESTTTTTTTTTT~g~");
+
+                if (!enabled && Game.Player.Character.IsInVehicle())
+                {
+                    Vehicle veh = Game.Player.Character.CurrentVehicle;
+
+                    if (veh != null && veh.Exists() && veh.IsAlive)
+                    {
+                        //enable radio when disable mod
+                        Function.Call(Hash.SET_VEHICLE_RADIO_ENABLED, veh, true);
+                    }
+                }
+            }
+
+            if (enabled && Game.Player.Character.IsInVehicle())
+            {
+                Vehicle veh = Game.Player.Character.CurrentVehicle;
+
+                if (veh != null && veh.Exists() && veh.IsAlive)
+                {
+                    Function.Call(Hash.SET_VEHICLE_RADIO_ENABLED, Game.Player.Character.CurrentVehicle, false);
                 }
             }
         }
